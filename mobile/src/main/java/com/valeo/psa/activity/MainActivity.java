c31c5b4d57e6280515131d69899ac7e508f53054
l-avaratha <arun.varatharajan.ext@valeo.com>
2017-01-09 10:57:26 +0100

    [FIX] Change 208 car image to ds5 one

--- mobile/src/main/java/com/valeo/psa/activity/MainActivity.java
+++ mobile/src/main/java/com/valeo/psa/activity/MainActivity.java
@@ -420,7 +420,7 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerAdapter
                 new NotificationCompat.WearableExtender()
                         .addAction(action)
                         .setBackground(BitmapFactory
-                                .decodeResource(getResources(), R.mipmap.car_model_208)));
+                                .decodeResource(getResources(), R.mipmap.car_model_ds5)));
         notificationManager.notify(notifId, notificationBuilder.build());
     }
 
@@ -564,7 +564,7 @@ public class MainActivity extends AppCompatActivity implements MyRecyclerAdapter
      */
     private List<Car> createCarList() {
         List<Car> resultList = new ArrayList<>(3);
-        resultList.add(new Car(R.mipmap.car_model_208, "1", getString(R.string.ds5), getString(R.string.VIN)));
+        resultList.add(new Car(R.mipmap.car_model_ds5, "1", getString(R.string.ds5), getString(R.string.VIN)));
         resultList.add(new Car(R.drawable.car_logo, "2", getString(R.string.peugeot_208), getString(R.string.VIN2)));
         return resultList;
     }
